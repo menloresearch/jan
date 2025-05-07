514610780cc818c4afa188a598299d49ad99e42b
Thien Tran <gau.nernst@yahoo.com.sg>
2025-05-07 13:06:22 +0700

    make it compile

--- src-tauri/src/core/state.rs
+++ src-tauri/src/core/state.rs
@@ -3,13 +3,14 @@ use std::{collections::HashMap, sync::Arc};
 use crate::core::utils::download::DownloadManagerState;
 use rand::{distributions::Alphanumeric, Rng};
 use rmcp::{service::RunningService, RoleClient};
-use tokio::sync::Mutex;
+use tokio::{process::Child, sync::Mutex};
 
 #[derive(Default)]
 pub struct AppState {
     pub app_token: Option<String>,
     pub mcp_servers: Arc<Mutex<HashMap<String, RunningService<RoleClient, ()>>>>,
     pub download_manager: Arc<Mutex<DownloadManagerState>>,
+    pub llama_server_process: Arc<Mutex<Option<Child>>>,
 }
 pub fn generate_app_token() -> String {
     rand::thread_rng()
